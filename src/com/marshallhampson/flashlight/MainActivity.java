53f8f5a37527f202fbb332f092341c7d3a8a626f
Marshall Hampson <hampson.mr@gmail.com>
2014-08-09 18:42:29 -0700

    Initial commit

---
+++ src/com/marshallhampson/flashlight/MainActivity.java
@@ -0,0 +1,79 @@
+package com.marshallhampson.flashlight;
+
+import android.support.v7.app.ActionBarActivity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	private Camera c;
+	private boolean on;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        c = null;
+        on = true;
+        try 
+		{
+    		c = Camera.open();
+    	}
+    	catch (Exception e) 
+    	{
+    	}
+		Parameters p = c.getParameters();
+		p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		c.setParameters(p);
+		c.startPreview();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
+    public void OnOffSwitchClick(View view) {
+    	on = !on;
+    	if (on)
+    	{
+    		try 
+    		{
+        		c = Camera.open();
+        	}
+        	catch (Exception e) 
+        	{
+        	}
+    		Parameters p = c.getParameters();
+    		p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+    		c.setParameters(p);
+    		c.startPreview();
+    	}
+    	else 
+    	{
+    		c.stopPreview();
+    		c.release();
+    	}
+    }
+}
